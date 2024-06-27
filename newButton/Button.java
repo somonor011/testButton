1d92489bfb97c3e0e529feb89a5a50dc1252d53e
Somonor Hong <somonorhong011@gmail.com>
2024-06-27 18:37:52 +0700

    add file

---
+++ newButton/Button.java
@@ -0,0 +1,39 @@
+package newButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Button extends JFrame{
+   
+     Button(){
+         JButton btn1 = new JButton();
+         btn1.setBounds(10, 300, 200, 100);
+         btn1.setText("Button demo");
+         btn1.setFocusable(false);
+
+         JButton btn2 = new JButton();
+         btn2.setBounds(210, 300, 200, 100);
+         btn2.setText("Hello world");
+         btn2.setFocusable(false);
+
+         JButton btn3 = new JButton();
+         btn3.setBounds(410, 300, 200, 100);
+         btn3.setText("Welcome to Java");
+         btn3.setFocusable(false);
+
+         JButton btn4 = new JButton();
+         btn4.setBounds(610, 300, 200, 100);
+         btn4.setText("Program");
+         btn4.setFocusable(false);
+
+         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         this.setVisible(true);
+         this.setSize(1000, 500);
+         this.setLayout(null);
+         this.add(btn1);
+         this.add(btn2);
+         this.add(btn3);
+         this.add(btn4);
+     }
+       
+    
+}
